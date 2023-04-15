0271dd48ac39b1074ecf3c41db8004b4bc472d69
IRONDESK <zerozerobase@gmail.com>
2023-04-16 01:31:29 +0900

    [Init] setting initial page and style setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import "../styles/globals.css";
 import { Provider } from "jotai";
 import type { AppProps } from "next/app";
 
+import Layout from "../components/Layouts";
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider>
-            <Component {...pageProps} />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
         </Provider>
     );
 }

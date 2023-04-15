a301d0565a1d0a30ba64f830e915d32813235e98
IRONDESK <zerozerobase@gmail.com>
2023-04-16 01:29:57 +0900

    [Route] add new pages about rank

---
+++ src/pages/rank/[id]/player.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LocalPlayerRank() {
+    return <div>PlayerRank</div>;
+}
+
+export default LocalPlayerRank;
---
+++ src/pages/rank/[id]/team.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LocalTeamRank() {
+    return <div>LocalRank</div>;
+}
+
+export default LocalTeamRank;

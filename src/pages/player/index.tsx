66ea0760719656865d8fad1da50ac548e5443e97
IRONDESK <zerozerobase@gmail.com>
2023-06-10 15:42:41 +0900

    [Fix] wrapping router.push avoid rendering error

--- src/pages/player/index.tsx
+++ src/pages/player/index.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAtom } from "jotai";
 import { useRouter } from "next/router";
 
 import { loggedState } from "@/src/atoms/state";
 
 function PlayerIndex() {
-    const [logged, setLogged] = useAtom(loggedState);
+    const [logged] = useAtom(loggedState);
     const router = useRouter();
 
     if (logged.username) {
-        router.push(`/player/${logged.username}`);
+        useEffect(() => {
+            router.push(`/player/${logged.username}`);
+        }, []);
+
         return <div></div>;
     } else {
-        router.push("/user/login");
+        useEffect(() => {
+            router.push("/user/login");
+        }, []);
+
         return <div></div>;
     }
 }

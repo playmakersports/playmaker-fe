9da2d4ddc2ce83f842f01b6d0903edd3e1a1e3fa
IRONDESK <zerozerobase@gmail.com>
2023-06-10 15:08:56 +0900

    [Feat] 로그인 화면 구현

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import { useAtom } from "jotai";
+import styled from "@emotion/styled";
+
+import InputBox, { IInputBoxDataType } from "../../components/Common/InputBox";
+import ButtonLarge from "@/src/components/Common/ButtonLarge";
+import { loggedState } from "@/src/atoms/state";
 
 function Login() {
-    return <div>Login</div>;
+    const {
+        register,
+        handleSubmit,
+        watch,
+        setFocus,
+        formState: { errors },
+    } = useForm();
+    const router = useRouter();
+    const [, setLogged] = useAtom(loggedState);
+
+    const LOGIN_INPUTS: IInputBoxDataType[] = [
+        { id: "username", label: "아이디", type: "text" },
+        { id: "password", label: "비밀번호", type: "password" },
+    ];
+
+    const onSubmit = (data: FieldValues) => {
+        console.log(data);
+        setLogged({ username: data.username, nickname: "이강인이피엘가자" });
+        router.push("/");
+    };
+
+    useEffect(() => {
+        setFocus("username");
+    }, []);
+
+    return (
+        <Container onSubmit={handleSubmit(onSubmit)}>
+            {LOGIN_INPUTS.map((value) => (
+                <InputBox
+                    key={value.id}
+                    register={register}
+                    watch={watch}
+                    id={value.id}
+                    label={value.label}
+                    type={value.type}
+                    required={true}
+                    errors={errors}
+                />
+            ))}
+            <ButtonLarge type="submit" main={true} shadow={false} text="로그인" />
+            <ButtonLarge
+                type="button"
+                main={false}
+                shadow={false}
+                text="회원가입"
+                callback={() => router.push("/user/join")}
+            />
+        </Container>
+    );
 }
+
+const Container = styled.form`
+    display: flex;
+    flex-direction: column;
+    padding: 32px 16px;
+    gap: 16px;
+`;
 
 export default Login;

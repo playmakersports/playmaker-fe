c7adc9ef2e1a9c5cf606910e409bfd41c9e0fac1
IRONDESK <zerozerobase@gmail.com>
2023-06-10 15:11:59 +0900

    [Feat] 팀 페이지 구현

--- src/pages/team/room/[id]/index.tsx
+++ src/pages/team/room/[id]/index.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
 
 import FloatBottom from "@/src/components/Common/FloatBottom";
 import ButtonLarge from "@/src/components/Common/ButtonLarge";
+import Card from "@/src/components/Main/Card";
 
 function Team() {
+    const router = useRouter();
+    const teamId = router.query.id;
     const teamColor = "#237c50";
+
+    const TEAM_BAORD_MOCK = [
+        { articleId: "123", label: "공지", title: "1주년 기념", writtenAt: "2023.06.05" },
+        { articleId: "133", label: "", title: "게시판 제목1", writtenAt: "2023.05.05" },
+        { articleId: "143", label: "", title: "게시판 제목2", writtenAt: "2023.05.05" },
+    ];
+
     return (
         <Container>
             <NameBox color={teamColor}>
                 <NameHead>
-                    <Name>축구 FC</Name>
-                    <p>관리</p>
+                    <Name>팀이름 FC</Name>
+                    <button className="control-icon" onClick={() => router.push(`/team/room/${teamId}/manager`)}>
+                        매니저
+                    </button>
                 </NameHead>
                 <TeamInfo>
                     <p className="team-info-number">
@@ -21,7 +34,34 @@ function Team() {
                     <p className="numbers team-info-founded">2020.12.30</p>
                 </TeamInfo>
             </NameBox>
-            1
+            <Cards>
+                <Card title="팀 게시판" link={`/team/room/${teamId}/board`}>
+                    <BoardCard>
+                        {TEAM_BAORD_MOCK.map((item) => (
+                            <li key={item.articleId}>
+                                <span className="card-board-title">
+                                    {item.label && <span className="card-board-label">{item.label}</span>}
+                                    {item.title}
+                                </span>
+                                <span className="card-board-written numbers">{item.writtenAt}</span>
+                            </li>
+                        ))}
+                    </BoardCard>
+                </Card>
+                <Card title="사진 게시판" link={`/team/room/${teamId}/photos`}>
+                    <BoardCard>
+                        {TEAM_BAORD_MOCK.map((item) => (
+                            <li key={item.articleId}>
+                                <span className="card-board-title">
+                                    {item.label && <span className="card-board-label">{item.label}</span>}
+                                    {item.title}
+                                </span>
+                                <span className="card-board-written numbers">{item.writtenAt}</span>
+                            </li>
+                        ))}
+                    </BoardCard>
+                </Card>
+            </Cards>
             <FloatBottom>
                 <>
                     <ButtonLarge callback={() => console.log("hi")} main={false} text="경기 목록" />
@@ -32,20 +72,17 @@ function Team() {
     );
 }
 
-const defaultFontFamily =
-    "system-ui, -apple-system, Pretendard, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
-
 const Container = styled.section`
-    padding: 160px 16px 0;
+    padding: 136px 16px 0;
 `;
 
 const NameBox = styled.article<{ color: string }>`
     position: fixed;
-    padding: 48px 32px 24px;
+    padding: 40px 28px 24px;
     top: 0;
     left: 0;
     width: 100%;
-    height: 184px;
+    height: 168px;
     background-color: ${(props) => props.color ?? "silver"};
     z-index: 10;
     &::after {
@@ -54,10 +91,10 @@ const NameBox = styled.article<{ color: string }>`
         margin-left: -4px;
         content: "";
         left: 0;
-        bottom: -35px;
+        bottom: -28px;
         background-color: ${(props) => props.color ?? "silver"};
         width: calc(100% + 6px);
-        height: 36px;
+        height: 28px;
         clip-path: polygon(50% 100%, 0 0, 100% 0);
     }
 `;
@@ -67,6 +104,26 @@ const NameHead = styled.div`
     width: 100%;
     justify-content: space-between;
     align-items: center;
+    .control-icon {
+        display: flex;
+        padding: 6px 12px;
+        align-items: center;
+        gap: 2px;
+        color: #000;
+        font-size: 0.85rem;
+        font-weight: 500;
+        border-radius: 20px;
+        background-color: #fff;
+        &::before {
+            content: "";
+            width: 20px;
+            height: 20px;
+            background-image: url("/assets/icons/control_icon.svg");
+            background-size: 18px;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+    }
 `;
 const Name = styled.h3`
     background: none;
@@ -78,7 +135,7 @@ const Name = styled.h3`
 `;
 const TeamInfo = styled.div`
     display: flex;
-    margin: 24px 0 0;
+    margin: 20px 2px 0;
     justify-content: space-between;
     align-items: top;
     color: #fff;
@@ -89,40 +146,82 @@ const TeamInfo = styled.div`
         span:first-of-type {
             margin-top: -3px;
             font-size: 2.9rem;
-            font-weight: 400;
+            font-weight: 500;
             &::after {
                 display: block;
                 content: "포인트";
                 margin: 2px 0 0;
-                font-size: 0.95rem;
-                font-weight: 300;
-                font-family: ${defaultFontFamily};
+                font-size: 0.8rem;
+                letter-spacing: -0.5px;
+                font-weight: 400;
+                font-family: inherit;
+                opacity: 0.75;
             }
         }
         span:last-of-type {
             font-size: 1.8rem;
-            font-weight: 400;
+            font-weight: 500;
             &::after {
                 display: block;
                 content: "동네랭킹";
                 margin: 3px 0 0;
-                font-size: 0.95rem;
-                font-weight: 300;
-                font-family: ${defaultFontFamily};
+                font-size: 0.8rem;
+                letter-spacing: -0.5px;
+                font-weight: 400;
+                font-family: inherit;
+                opacity: 0.75;
             }
         }
     }
     .team-info-founded {
-        font-size: 1.6rem;
+        font-size: 1.45rem;
         letter-spacing: -0.1px;
         &::after {
             display: block;
             margin: 3px 0 0;
             content: "창단";
-            font-weight: 300;
-            font-family: ${defaultFontFamily};
-            font-size: 0.95rem;
+            font-size: 0.8rem;
+            letter-spacing: -0.5px;
+            font-weight: 400;
+            font-family: inherit;
             text-align: right;
+            opacity: 0.75;
+        }
+    }
+`;
+
+const Cards = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+const BoardCard = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    li {
+        display: flex;
+        padding: 4px 0;
+        align-items: center;
+        justify-content: space-between;
+        gap: 8px;
+        .card-board-title {
+            display: inline-flex;
+            align-items: center;
+            gap: 4px;
+            font-size: 0.95rem;
+            line-height: 1.1rem;
+        }
+        .card-board-label {
+            padding: 0 6px;
+            border-radius: 20px;
+            border: 1px solid var(--black);
+            font-size: 0.7rem;
+            font-weight: 500;
+        }
+        .card-board-written {
+            opacity: 0.6;
+            font-size: 0.85rem;
         }
     }
 `;

012bbc9007bf4df297eea790ec41747bfdef74e0
IRONDESK <zerozerobase@gmail.com>
2023-07-29 15:22:00 +0900

    [Docs] 공통 스타일 및 svg package 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,16 @@
 export const LightTheme = {
     color: {
         main: "#adef2b",
+        main2: "#d0f28c",
         background: "#f5f5f5",
+        lightGray: "#f0f0f0",
         gray1: "#e1e1e1",
-        gray2: "#A5ABB1",
-        gray3: "#6F7880",
-        gray4: "#5b5d5f",
+        gray2: "#adadad",
+        gray3: "#7d7d7d",
+        gray4: "#5c5d5e",
         warn: "#ff5100",
         white: "#fff",
+        black: "#000",
     },
     filter: {
         invert: "invert(0)",
@@ -17,13 +20,16 @@ export const LightTheme = {
 export const DarkTheme = {
     color: {
         main: "#adef2b",
+        main2: "#d0f28c",
         background: "#212121",
+        lightGray: "#f0f0f0",
         gray1: "#464646",
-        gray2: "#A5ABB1",
+        gray2: "#adadad",
         gray3: "#878e94",
         gray4: "#8a8a8a",
         warn: "#ff5100",
         white: "#161617",
+        black: "#fff",
     },
     filter: {
         invert: "invert(1)",

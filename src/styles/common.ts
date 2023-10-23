b501647a26b4b1951333e9c441f01c844e5a7237
IRONDESK <zerozerobase@gmail.com>
2023-10-23 21:01:46 +0900

    [Fix] 글자 설정 수정 (폰트, base-font-size 등)

---
+++ src/styles/common.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const TitleText = styled.p`
+    font-size: 2rem;
+    font-weight: 600;
+    font-family: SUITE Variable;
+`;
+export const SubTitleText = styled.p`
+    font-size: 1.6rem;
+    font-weight: 600;
+    font-family: SUITE Variable;
+`;
+export const MdHeadText = styled.p`
+    font-size: 1.4rem;
+    font-weight: 600;
+    font-family: SUITE Variable;
+`;
+export const MdText = styled.p`
+    font-size: 1.4rem;
+`;

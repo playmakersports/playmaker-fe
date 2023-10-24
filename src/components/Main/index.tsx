47348512f2cf5ae704104cdcc4883f623ba9334c
IRONDESK <zerozerobase@gmail.com>
2023-10-24 22:35:00 +0900

    [Feat] 캐러셀형 카드 컴포넌트 구현 및 적용

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,24 +4,26 @@ import styled from "@emotion/styled";
 import AlertList from "./AlertList";
 import RankCard from "./RankCard";
 import FindPlayerCard from "./FindPlayerCard";
+import RankCarousel from "./RankCarousel";
 
 function Main() {
     return (
         <>
             <AlertList />
             <Articles>
-                <RankCard
+                <RankCarousel
                     type="TEAM"
                     localId="anyang"
-                    localname="안양시"
+                    localName="안양시"
                     list={[
-                        { rank: 1, name: "불도저FC", point: 230 },
-                        { rank: 2, name: "불도저FC", point: 230 },
-                        { rank: 3, name: "불도저FC", point: 230 },
-                        { rank: 4, name: "불도저FC", point: 230 },
-                        { rank: 5, name: "불도저FC", point: 230 },
+                        { rank: 1, teamId: "1231", teamName: "불도저FC", teamImage: "", point: 230, winRate: 0.4 },
+                        { rank: 2, teamId: "1232", teamName: "진짜FC", teamImage: "", point: 230, winRate: 0.4 },
+                        { rank: 3, teamId: "1233", teamName: "레알FC", teamImage: "", point: 230, winRate: 0.4 },
+                        { rank: 4, teamId: "1234", teamName: "맨시티FC", teamImage: "", point: 230, winRate: 0.4 },
+                        { rank: 5, teamId: "1235", teamName: "블루드래곤FC", teamImage: "", point: 230, winRate: 0.4 },
                     ]}
                 />
+
                 <FindPlayerCard
                     localId="anyang"
                     localname="안양시"
@@ -34,7 +36,7 @@ function Main() {
                 <RankCard
                     type="PLAYER"
                     localId="anyang"
-                    localname="안양시"
+                    localName="안양시"
                     list={[
                         { rank: 1, name: "안양의메시", point: 230 },
                         { rank: 2, name: "안양의메시", point: 230 },
@@ -50,6 +52,7 @@ function Main() {
 
 const Articles = styled.section`
     display: flex;
+    margin: 32px 0 0;
     padding: 0 16px;
     flex-direction: column;
     gap: 24px;

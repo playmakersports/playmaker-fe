0181cddf55c97e18be6a3062d2b504267ea19625
IRONDESK <zerozerobase@gmail.com>
2023-10-24 22:40:50 +0900

    [Fix] 외부 폰트 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
     return (
         <Html lang="ko">
             <Head />
+            <link
+                href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/variable/woff2/SUIT-Variable.css"
+                rel="stylesheet"
+            />
+            <link
+                href="https://cdn.jsdelivr.net/gh/sun-typeface/SUITE/fonts/variable/woff2/SUITE-Variable.css"
+                rel="stylesheet"
+            />
             <meta name="theme-color" media="(prefers-color-scheme: light)" content="#f5f5f5" />
             <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#212121" />
             <body>

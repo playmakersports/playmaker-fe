1559563fc001991e97ff545b18abe690de6a5b77
IRONDESK <zerozerobase@gmail.com>
2023-10-26 21:34:21 +0900

    [Feat] 시간 관련 공통 함수 추가

--- src/util/time.ts
+++ src/util/time.ts
@@ -12,3 +12,34 @@ export function getCurrentDateTime(format?: "date" | "time" | "dateTime") {
     if (format === "time") return `${hours}:${minutes}`;
     return formattedDateTime;
 }
+
+export const getDateInterval = (target: string, target2?: string) => {
+    const today = target2 ? new Date(target2) : new Date();
+    const targetDate = new Date(target.split(" ")[0]);
+
+    // 입력된 날짜와 현재 날짜 사이의 차이 계산
+    const timeDiff = targetDate.getTime() - today.getTime();
+    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    return diffDays;
+};
+
+export const getDateDiffMessage = (diffDays: number, targetDate?: Date) => {
+    const target = targetDate ?? new Date();
+
+    if (diffDays === 0) {
+        return `오늘`;
+    } else if (diffDays === 1) {
+        return `내일 `;
+    } else if (diffDays === 2) {
+        return `2일 후`;
+    } else if (diffDays === 3) {
+        return `3일 후`;
+    } else if (diffDays >= 4) {
+        const month = target.getMonth() + 1;
+        const day = target.getDate();
+        return `${month}월 ${day}일 `;
+    } else {
+        return null;
+    }
+};

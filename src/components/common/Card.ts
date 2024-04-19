d6c3f60df0b1b88041da52be67cc2d0c8170b297
IRONDESK <zerozerobase@gmail.com>
2024-04-19 14:04:26 +0900

    [Fix] 카드 공통 색상 수정

--- src/components/common/Card.ts
+++ src/components/common/Card.ts
@@ -3,12 +3,12 @@ import styled from "@emotion/styled";
 export const BasicWhiteCard = styled.div`
   padding: 20px;
   border-radius: 16px;
-  background-color: ${({ theme }) => theme.white};
+  background-color: ${({ theme }) => theme.card};
   user-select: none;
 `;
 
 export const BasicWhiteSection = styled.div`
   padding: 16px;
-  background-color: ${({ theme }) => theme.white};
+  background-color: ${({ theme }) => theme.card};
   user-select: none;
 `;

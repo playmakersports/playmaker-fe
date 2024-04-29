1de1dc5137422dbc86c56b22d080845fb1896c7b
IRONDESK <zerozerobase@gmail.com>
2024-04-29 21:54:14 +0900

    [Fix] PWA 관련 설정 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,10 @@ export default function Document() {
           content="width=device-width, initial-scale=1, minimum-scale=1,  maximum-scale=1, user-scalable=no"
         />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="플메" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0e0e0e" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#101010" />
         <link rel="apple-touch-icon" sizes="192x192" href="/images/icons/icon-192x192.png" />
         <link rel="apple-touch-icon" sizes="512x512" href="/images/icons/icon-512x512.png" />
         <link

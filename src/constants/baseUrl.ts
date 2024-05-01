a1d2e31c138abe44ec7ef6f3e6ae89d6e422ad25
IRONDESK <zerozerobase@gmail.com>
2024-05-01 17:28:49 +0900

    [Fix] 카카오 로그인 구현 관련 수정

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,1 +1,4 @@
+// 프론트
 export const VERCEL_BASE_URL = "https://playmaker-fe.vercel.app";
+// 백엔드
+export const BACK_END_REQUEST_URL = "";
--- src/pages/user/login/kakao.tsx
+++ src/pages/user/login/kakao.tsx
@@ -1,12 +1,38 @@
-import React from "react";
 import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import { BACK_END_REQUEST_URL } from "@/constants/baseUrl";
 
 function KakaoLogin() {
   const router = useRouter();
+  const [result, setResult] = useState("");
   const KAKAO_API_CODE = router.query.code;
-  console.log(KAKAO_API_CODE);
+  const target = `${BACK_END_REQUEST_URL}/login/oauth2?code=${KAKAO_API_CODE}`;
 
-  return <div>KakaoLogin</div>;
+  useEffect(() => {
+    if (KAKAO_API_CODE) {
+      axios.get(target).then((res) => setResult(JSON.stringify(res)));
+    }
+  }, [router.query]);
+
+  return (
+    <div style={{ fontSize: "1.8rem", lineHeight: "3.2rem" }}>
+      response:
+      <code
+        style={{
+          display: "block",
+          margin: "0 20px",
+          padding: "12px",
+          fontFamily: "monospace",
+          background: "#ececec",
+          wordBreak: "break-all",
+        }}
+      >
+        {result}
+      </code>
+    </div>
+  );
 }
 
 export default KakaoLogin;

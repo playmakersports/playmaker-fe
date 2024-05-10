33ba4e59461005f543bd1e4ed605c070cfe0d57a
IRONDESK <zerozerobase@gmail.com>
2024-05-10 17:05:34 +0900

    [Fix] 공통 요소 추가

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -1,10 +1,10 @@
 export const FONTS = {
   HEAD1: `font-weight: 700; font-size: 2rem; line-height: 2.2rem;`,
   HEAD2: `font-weight: 600; font-size: 1.6rem; line-height: 2rem;`,
-  MD1: `font-weight: 600; font-size: 1.6rem; line-height: 2rem;`,
-  MD1W500: `font-weight: 500; font-size: 1.6rem; line-height: 2rem;`,
-  MD2: `font-weight: 500; font-size: 1.4rem; line-height: 1.8rem;`,
-  MD3: `font-weight: 500; font-size: 1.2rem; line-height: 1.6rem;`,
+  MD1: `font-weight: 600; font-size: 1.6rem; line-height: 2.4rem;`,
+  MD1W500: `font-weight: 500; font-size: 1.6rem; line-height: 2.4rem;`,
+  MD2: `font-weight: 500; font-size: 1.4rem; line-height: 2rem;`,
+  MD3: `font-weight: 500; font-size: 1.2rem; line-height: 1.8rem;`,
 };
 
 export const SCROLL_HIDE = `
@@ -13,3 +13,32 @@ export const SCROLL_HIDE = `
     &::-webkit-scrollbar {
       display: none;
 }`;
+
+export const CARD_ACTIVE = `
+    border-radius: 16px;
+    transition: all 0.2s;
+    &:active {
+      transform: scale(0.97);
+      background-color: var(--card-active);
+    }
+`;
+export const BUTTON_ACTIVE = `
+    border-radius: 16px;
+    transition: all 0.2s;
+    &:active {
+      transform: scale(0.97);
+      background-color: var(--background);
+    }
+`;
+
+export const INNER_BUTTON_ACTIVE = `
+    border-radius: 16px;
+    transition: all 0.2s;
+    &:active {
+      background-color: var(--background);
+    }
+    &:active > .inner-button {
+      transition: transform 0.2s;
+      transform: scale(0.97);
+    }
+`;

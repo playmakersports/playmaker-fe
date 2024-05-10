98a5f5632a39a72f1db50906f184187ea76c4140
IRONDESK <zerozerobase@gmail.com>
2024-05-10 17:02:29 +0900

    [Feat] 달력 Hook 구현

---
+++ src/hook/useCalendar.tsx
@@ -0,0 +1,129 @@
+import {
+  getDaysInMonth,
+  subMonths,
+  getWeeksInMonth,
+  getDay,
+  startOfMonth,
+  endOfMonth,
+  subDays,
+  addDays,
+  isSunday,
+} from "date-fns";
+import { useState } from "react";
+
+const DAY_OF_WEEK = 7;
+const DAY_LIST = ["일", "월", "화", "수", "목", "금", "토"];
+
+function useCalendar(defaultDate?: Date) {
+  const [currentDate, setCurrentDate] = useState(defaultDate ?? new Date());
+  const totalMonthDays = getDaysInMonth(currentDate);
+  const calendarLength = getWeeksInMonth(currentDate, { weekStartsOn: 0 }) * DAY_OF_WEEK;
+
+  const prevMonthDayListLength = Math.max(0, getDay(startOfMonth(currentDate)));
+  const prevDayList = Array.from({
+    length: prevMonthDayListLength,
+  }).map((_, index) => {
+    const targetDate = subDays(endOfMonth(subMonths(currentDate, 1)), prevMonthDayListLength - index - 1);
+    return {
+      date: targetDate,
+      displayValue: subDays(endOfMonth(subMonths(currentDate, 1)), prevMonthDayListLength - index - 1).getDate(),
+      previousMonth: true,
+      nextMonth: false,
+      holiday: {
+        isHoliday: isSunday(targetDate) || !!getKoreanHolidays(targetDate),
+        holidayName: getKoreanHolidays(targetDate) ?? null,
+      },
+    };
+  });
+  const currentDayList = Array.from({ length: totalMonthDays }).map((_, index) => {
+    const targetDate = addDays(`${currentDate.getFullYear()}/${currentDate.getMonth() + 1}/01`, index);
+    return {
+      date: targetDate,
+      displayValue: index + 1,
+      previousMonth: false,
+      nextMonth: false,
+      holiday: {
+        isHoliday: isSunday(targetDate) || !!getKoreanHolidays(targetDate),
+        holidayName: getKoreanHolidays(targetDate) ?? null,
+      },
+    };
+  });
+  const nextDayList = Array.from({
+    length: calendarLength - currentDayList.length - prevDayList.length,
+  }).map((_, index) => {
+    const targetDate = addDays(
+      `${currentDate.getFullYear()}/${currentDate.getMonth() + 1}/${totalMonthDays}`,
+      index + 1
+    );
+    return {
+      date: targetDate,
+      displayValue: index + 1,
+      previousMonth: false,
+      nextMonth: true,
+      holiday: {
+        isHoliday: isSunday(targetDate) || !!getKoreanHolidays(targetDate),
+        holidayName: getKoreanHolidays(targetDate) ?? null,
+      },
+    };
+  });
+
+  const currentCalendarList = prevDayList.concat(currentDayList, nextDayList);
+  const weekCalendarList = currentCalendarList.reduce((acc: WeekDateObject[][], cur, idx) => {
+    const chunkIndex = Math.floor(idx / DAY_OF_WEEK);
+    if (!acc[chunkIndex]) {
+      acc[chunkIndex] = [];
+    }
+    acc[chunkIndex].push(cur);
+    return acc;
+  }, []);
+
+  return {
+    dayList: DAY_LIST,
+    weekCalendarList,
+    currentDate,
+    currentDateHoliday: {
+      isHoliday: isSunday(currentDate) || !!getKoreanHolidays(currentDate),
+      holidayName: getKoreanHolidays(currentDate),
+    },
+    setCurrentDate,
+  };
+}
+
+type WeekDateObject = {
+  date: Date;
+  displayValue: number;
+  previousMonth: boolean;
+  nextMonth: boolean;
+  holiday: {
+    isHoliday: boolean;
+    holidayName: string | null;
+  };
+};
+
+function getKoreanHolidays(target: Date) {
+  const MMdd = `${(target.getMonth() + 1).toString().padStart(2, "0")}-${target.getDate().toString().padStart(2, "0")}`;
+  const yyyyMMdd = `${target.getFullYear()}-${MMdd}`;
+
+  const HOLIDAYS_LIST: Record<string, string> = {
+    "01-01": "신정",
+    "03-01": "3·1절",
+    "05-05": "어린이날",
+    "06-06": "현충일",
+    "08-15": "광복절",
+    "10-03": "개천절",
+    "10-09": "한글날",
+    "12-25": "성탄절",
+  };
+
+  const SPECIAL_HOLIDAYS_LIST: Record<string, string> = {
+    "2024-04-10": "국회의원 선거일",
+    "2024-05-06": "어린이날 대체공휴일",
+    "2024-09-16": "추석 연휴",
+    "2024-09-17": "추석",
+    "2024-09-18": "추석 연휴",
+  };
+
+  return HOLIDAYS_LIST[MMdd] ?? SPECIAL_HOLIDAYS_LIST[yyyyMMdd];
+}
+
+export default useCalendar;

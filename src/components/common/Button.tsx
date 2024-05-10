86ff648ed150d11bfc16ef7f014b5f4e4992441d
IRONDESK <zerozerobase@gmail.com>
2024-05-10 16:49:15 +0900

    [Fix] 버튼 컴포넌트 고도화, WARN 스타일 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,10 +3,12 @@ import styled from "@emotion/styled";
 import { useTheme } from "@emotion/react";
 import { FONTS } from "@/styles/common";
 
+export type ButtonStyleMode = "MAIN" | "OPTION1" | "OPTION2" | "SUB1" | "SNS_LOGIN" | "WARN";
 type Props = {
-  mode: "MAIN" | "OPTION1" | "OPTION2" | "SUB1" | "SNS_LOGIN";
+  mode: ButtonStyleMode;
   children: ReactNode;
   fullWidth?: boolean;
+  autoHeight?: boolean;
   type?: "button" | "submit" | "reset" | undefined;
   flex?: number;
   disabled?: boolean;
@@ -19,11 +21,11 @@ type Props = {
 
 function Button(props: Props) {
   const theme = useTheme();
-  const { split, mode, children, fullWidth, type, flex, disabled = false, onClick } = props;
+  const { split, mode, children, fullWidth, autoHeight = false, type, flex, disabled = false, onClick } = props;
   const BUTTON_STYLE = {
     MAIN: {
       background: theme.main,
-      color: theme.white,
+      color: "#fff",
     },
     OPTION1: {
       background: theme.gray1,
@@ -34,6 +36,10 @@ function Button(props: Props) {
       color: theme.black,
     },
     SUB1: {
+      background: theme.sub1,
+      color: "#fff",
+    },
+    WARN: {
       background: theme.sub1,
       color: "#fff",
     },
@@ -52,6 +58,7 @@ function Button(props: Props) {
         flex={flex}
         fullWidth={fullWidth}
         disabled={disabled}
+        autoHeight={autoHeight}
       >
         {children}
       </SnsWrapper>
@@ -66,6 +73,7 @@ function Button(props: Props) {
         mode={BUTTON_STYLE[mode]}
         fullWidth={fullWidth}
         disabled={disabled}
+        autoHeight={autoHeight}
       >
         {children}
       </Wrapper>
@@ -73,7 +81,7 @@ function Button(props: Props) {
   }
   if (split) {
     return (
-      <SplitWrapper as="div" mode={BUTTON_STYLE[mode]} flex={flex} fullWidth={fullWidth}>
+      <SplitWrapper as="div" mode={BUTTON_STYLE[mode]} flex={flex} fullWidth={fullWidth} autoHeight={autoHeight}>
         <SplitButton disabled={disabled} type={type ?? "button"} onClick={onClick}>
           {children}
         </SplitButton>
@@ -89,22 +97,24 @@ type ButtonStyled = {
   mode: Record<string, string>;
   fullWidth?: boolean;
   flex?: number;
+  autoHeight?: boolean;
 };
 const Wrapper = styled.button<ButtonStyled>`
   display: inline-flex;
   align-items: center;
   justify-content: center;
+  padding: 6px 0;
   ${({ flex }) => (flex ? `flex: ${flex}` : "")};
   width: ${({ fullWidth }) => (fullWidth ? "100%" : "auto")};
-  height: 56px;
+  height: ${({ autoHeight }) => (autoHeight ? "auto" : "56px")};
   border: 0 solid transparent;
-  border-radius: 12px;
+  border-radius: ${({ autoHeight }) => (autoHeight ? "6px" : "12px")};
   background-color: ${({ mode }) => mode.background};
   color: ${({ mode }) => mode.color};
   white-space: nowrap;
   user-select: none;
   -webkit-font-smoothing: antialiased;
-  transition: filter 0.2s;
+  transition: filter 0.2s, transform 0.2s;
   ${FONTS.MD1};
   &:focus {
     outline: none;
@@ -115,6 +125,7 @@ const Wrapper = styled.button<ButtonStyled>`
   }
   &:active {
     filter: brightness(1.05);
+    transform: scale(0.98);
   }
 `;
 
@@ -126,6 +137,7 @@ const SnsWrapper = styled(Wrapper)`
 `;
 
 const SplitWrapper = styled(Wrapper)`
+  padding: 0;
   background: none;
   gap: 2px;
   overflow: hidden;
@@ -141,9 +153,11 @@ const SplitWrapper = styled(Wrapper)`
   }
   &:active {
     filter: none;
+    transform: none;
   }
 `;
 const SplitButton = styled.button`
+  padding: 6px 0;
   height: 100%;
   ${FONTS.MD1};
   &:focus {
@@ -155,6 +169,7 @@ const SplitButton = styled.button`
   }
   &:active {
     filter: brightness(1.05);
+    transform: none;
   }
 
   &:first-of-type {

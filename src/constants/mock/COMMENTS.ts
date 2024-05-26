9acce3c70a9b4c645bc6866084a602969871825f
IRONDESK <zerozerobase@gmail.com>
2024-05-26 14:30:41 +0900

    [Feat] mock data 상수 파일 분리

---
+++ src/constants/mock/COMMENTS.ts
@@ -0,0 +1,86 @@
+export const COMMENTS_MOCK = [
+  {
+    name: "홍길동",
+    writtenAt: "2024-05-24T18:32:32",
+    editable: true,
+    deletable: false,
+    contents: "댓글 내용입니다. 댓글 내용입니다.",
+    reply: [
+      {
+        name: "김길동",
+        writtenAt: "2024-05-24T18:40:32",
+        editable: false,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+      {
+        name: "박길동",
+        writtenAt: "2024-05-24T18:42:32",
+        editable: false,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+      {
+        name: "박길동",
+        writtenAt: "2024-05-24T19:01:12",
+        editable: false,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+      {
+        name: "홍길동",
+        writtenAt: "2024-05-24T21:20:07",
+        editable: true,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+    ],
+  },
+  {
+    name: "홍길동",
+    writtenAt: "2024-05-24T18:32:32",
+    editable: true,
+    deletable: false,
+    contents: "댓글 내용입니다. 댓글 내용입니다.",
+    reply: [
+      {
+        name: "박길동",
+        writtenAt: "2024-05-24T19:01:12",
+        editable: false,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+      {
+        name: "홍길동",
+        writtenAt: "2024-05-24T21:20:07",
+        editable: true,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+    ],
+  },
+  {
+    name: "홍길동",
+    writtenAt: "2024-05-24T18:32:32",
+    editable: true,
+    deletable: false,
+    contents: "댓글 내용입니다. 댓글 내용입니다.",
+    reply: [
+      {
+        name: "홍길동",
+        writtenAt: "2024-05-24T21:20:07",
+        editable: true,
+        deletable: false,
+        contents: "댓글 답글 내용입니다. 댓글 내용입니다.",
+      },
+    ],
+  },
+  {
+    name: "홍길동",
+    writtenAt: "2024-05-24T18:32:32",
+    editable: true,
+    deletable: false,
+    contents: "댓글 내용입니다. 댓글 내용입니다.",
+    reply: [],
+  },
+];
---
+++ src/constants/mock/SPORTS.ts
@@ -0,0 +1,7 @@
+export const SUPPORT_SPORTS = [
+  { value: "volley", name: "배구" },
+  { value: "basketball", name: "농구" },
+  { value: "football", name: "축구" },
+  { value: "badminton", name: "배드민턴" },
+  { value: "run", name: "러닝" },
+];

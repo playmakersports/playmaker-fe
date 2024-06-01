6781d23ac3566a7b2c4bc977ee46671707ab5079
IRONDESK <zerozerobase@gmail.com>
2024-06-01 16:03:42 +0900

    [Fix] 투표 종료시간, 복수, 익명 옵션 추가

--- src/components/Editor/Poll.tsx
+++ src/components/Editor/Poll.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { FormProvider, useFieldArray, useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { FONTS } from "@/styles/common";
 
 import SmallX from "@/assets/icon/editor/SmallX.svg";
 import Button from "../common/Button";
 import useToast from "@/hook/useToast";
-import RadioTab from "../common/RadioTab";
+import { InputCheckbox } from "../common/SelectInput";
+import { BasicInput } from "../common/Input";
+import { format } from "date-fns";
 
 function Poll() {
   const { trigger } = useToast();
+  const today = format(new Date(), "yyyy-MM-dd");
   const form = useForm({
     defaultValues: {
+      pollDue: false,
+      endDate: today,
+      endTime: "00:00",
       pollTitle: "",
       pollOptions: [{ value: "" }, { value: "" }],
     },
   });
-  const { register, control } = form;
+  const { register, watch, setValue, control } = form;
   const { fields, append, remove } = useFieldArray({ control, name: "pollOptions" });
 
   const removeOption = (index: number) => {
@@ -27,54 +33,70 @@ function Poll() {
     }
   };
 
+  console.log(watch());
+
   return (
-    <FormProvider {...form}>
-      <Container>
-        <Title
-          type="text"
-          placeholder="투표 제목을 입력하세요."
-          required
-          {...register("pollTitle", { required: true })}
-        />
-        <PollSetting>
-          <RadioTab
-            id="selectedType"
-            items={[
-              { value: "radio", name: "단일선택" },
-              { value: "checkbox", name: "복수선택" },
-            ]}
-          />
-          <RadioTab
-            id="anonymous"
-            items={[
-              { value: "no", name: "공개" },
-              { value: "yes", name: "익명" },
-            ]}
-          />
-        </PollSetting>
-        <Options>
-          {fields.map((field, index) => (
-            <Option key={field.id}>
-              <div className="option-index">{index + 1}</div>
-              <input type="text" placeholder="입력..." {...register(`pollOptions.${index}.value`)} />
-              <button onClick={() => removeOption(index)}>
-                <SmallX />
-              </button>
-            </Option>
-          ))}
-        </Options>
-        <Button
-          fullWidth
-          autoHeight
-          borderType
-          mode="OPTION1"
-          type="button"
-          onClick={() => append({ value: "" }, { shouldFocus: false })}
-        >
-          선택지 추가
-        </Button>
-      </Container>
-    </FormProvider>
+    <Container>
+      <BasicInput
+        control={control}
+        type="text"
+        name="pollTitle"
+        placeholder="투표 제목을 입력하세요."
+        delButton={() => setValue("pollTitle", "")}
+      />
+
+      <Options>
+        {fields.map((field, index) => (
+          <Option key={field.id}>
+            <div className="option-index">{index + 1}</div>
+            <input type="text" placeholder="입력..." {...register(`pollOptions.${index}.value`)} />
+            <button onClick={() => removeOption(index)}>
+              <SmallX />
+            </button>
+          </Option>
+        ))}
+      </Options>
+      <Button
+        fullWidth
+        autoHeight
+        mode="OPTION2"
+        type="button"
+        onClick={() => append({ value: "" }, { shouldFocus: false })}
+      >
+        선택지 추가
+      </Button>
+
+      <PollSetting>
+        <SetItem>
+          <InputCheckbox control={control} id="pollDue" name="pollDue" /> <label htmlFor="pollDue">종료</label>
+          <DueInputs>
+            <BasicInput
+              disabled={!watch("pollDue")}
+              medium
+              styleType="BORDER"
+              type="date"
+              name="endDate"
+              control={control}
+            />
+            <BasicInput
+              disabled={!watch("pollDue")}
+              medium
+              styleType="BORDER"
+              type="time"
+              name="endTime"
+              control={control}
+            />
+          </DueInputs>
+        </SetItem>
+        <SetItem>
+          <InputCheckbox control={control} id="multiple" name="multiple" /> <label htmlFor="multiple">복수선택</label>
+        </SetItem>
+        <SetItem>
+          <InputCheckbox control={control} id="anonymous" name="anonymous" />{" "}
+          <label htmlFor="anonymous">익명투표</label>
+        </SetItem>
+      </PollSetting>
+    </Container>
   );
 }
 
@@ -82,18 +104,35 @@ const Container = styled.div`
   width: 100%;
   margin-top: 20px;
 `;
+
 const PollSetting = styled.div`
   display: flex;
-  margin: 12px 0;
+  margin: 24px 0 12px;
+  padding-top: 20px;
+  gap: 20px 32px;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  border-top: 1px solid rgb(var(--gray-h4));
 `;
-const Title = styled.input`
-  width: 100%;
-  padding: 12px;
-  background-color: rgb(var(--gray-h6));
-  border-radius: 8px;
-  ${FONTS.MD1};
-  font-size: 1.8rem;
+const SetItem = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+  font-size: 1.5rem;
+  word-break: keep-all;
+
+  &:has(input:checked) {
+    color: var(--main);
+    font-weight: 600;
+  }
 `;
+
+const DueInputs = styled.div`
+  margin-left: 4px;
+  display: flex;
+  gap: 8px;
+`;
+
 const Options = styled.ul`
   display: flex;
   margin: 12px 0 20px;

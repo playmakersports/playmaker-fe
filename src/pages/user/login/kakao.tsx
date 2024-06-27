c97e36d504429b3dbd27e96c0fbac1d446ea7e75
IRONDESK <zerozerobase@gmail.com>
2024-06-27 12:37:53 +0900

    [Fix]  로딩, 에러 처리

--- src/pages/user/login/kakao.tsx
+++ src/pages/user/login/kakao.tsx
@@ -3,35 +3,50 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import { BACK_END_REQUEST_URL } from "@/constants/baseUrl";
+import { BaseContainer } from "@/components/common/Container";
+import Loading from "@/components/common/Loading";
 
 function KakaoLogin() {
   const router = useRouter();
   const [result, setResult] = useState("");
+  const [apiState, setApiState] = useState("");
   const KAKAO_API_CODE = router.query.code;
   const target = `${BACK_END_REQUEST_URL}/login/oauth2?code=${KAKAO_API_CODE}`;
 
   useEffect(() => {
+    setApiState("LOADING");
     if (KAKAO_API_CODE) {
-      axios.get(target).then((res) => setResult(JSON.stringify(res)));
+      axios
+        .get(target)
+        .then((res) => {
+          setApiState(res.status === 200 ? "SUCCESS" : "");
+          setResult(JSON.stringify(res));
+        })
+        .catch((err) => {
+          setApiState(`ERROR: ${err.code}`);
+        });
     }
   }, [router.query]);
 
   return (
-    <div style={{ fontSize: "1.8rem", lineHeight: "3.2rem" }}>
-      response:
-      <code
-        style={{
-          display: "block",
-          margin: "0 20px",
-          padding: "12px",
-          fontFamily: "monospace",
-          background: "#ececec",
-          wordBreak: "break-all",
-        }}
-      >
-        {result}
-      </code>
-    </div>
+    <BaseContainer>
+      {apiState === "LOADING" && <Loading />}
+      {apiState.split(":")[0] === "ERROR" && <div style={{ fontWeight: 800, fontSize: "1.6rem" }}>{apiState}</div>}
+      {apiState === "SUCCESS" && (
+        <code
+          style={{
+            display: "block",
+            marginTop: "12px",
+            padding: "12px",
+            fontFamily: "monospace",
+            background: "#ececec",
+            wordBreak: "break-all",
+          }}
+        >
+          {result}
+        </code>
+      )}
+    </BaseContainer>
   );
 }
 

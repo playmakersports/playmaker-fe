1a8f63f9265c7a6d85d54a7c7ec52df9766d5126
IRONDESK <zerozerobase@gmail.com>
2024-08-04 14:36:49 +0900

    [Fix] url 주소 변경

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,4 +1,4 @@
 // 프론트
 export const VERCEL_BASE_URL = "https://playermaker.vercel.app";
 // 백엔드
-export const BACK_END_REQUEST_URL = "https://port-0-playmaker-be-e9btb72blgj562lo.sel3.cloudtype.app:8080";
+export const BACK_END_REQUEST_URL = "https://port-0-playmaker-be-e9btb72blgj562lo.sel3.cloudtype.app";

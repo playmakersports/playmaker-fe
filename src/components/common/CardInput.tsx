9634c2cf5c14ed5d3d1a474f2427329825e7bf49
IRONDESK <zerozerobase@gmail.com>
2024-08-14 17:49:53 +0900

    [Feat] card 모양 checkbox, radio 구현

---
+++ src/components/common/CardInput.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import { BasicWhiteCard } from "./Card";
+import { FONTS } from "@/styles/common";
+
+type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "value"> & {
+  type: "checkbox" | "radio";
+  value: string;
+  children: React.ReactNode;
+};
+const CardInput = React.forwardRef<HTMLInputElement, Props>(({ type, value, children, id, ...rest }, ref) => (
+  <>
+    <Input type={type} ref={ref} id={id} value={value} {...rest} />
+    <Card htmlFor={id} role="button">
+      {children}
+    </Card>
+  </>
+));
+
+const Card = styled(BasicWhiteCard.withComponent("label"))`
+  cursor: pointer;
+  ${FONTS.MD1W500};
+  font-weight: 400;
+  transition: transform 0.2s;
+  &:active {
+    transform: scale(0.98);
+  }
+`;
+const Input = styled.input`
+  display: none;
+  &:checked + ${Card} {
+    font-weight: 500;
+    padding: 17px 19px; // border 2px 고려
+    border: 2px solid var(--main);
+    color: var(--main);
+  }
+`;
+
+export default CardInput;

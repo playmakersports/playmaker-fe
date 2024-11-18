1ed920a38aff340d7ba8c0a7bc62f8e782a5d49b
IRONDESK <zerozerobase@gmail.com>
2024-11-18 23:38:22 +0900

    fix. 회원가입 약관 모달 z-index 문제 수정

--- src/pages/user/apply/index.tsx
+++ src/pages/user/apply/index.tsx
@@ -10,7 +10,7 @@ import LoginWrapper from "@/components/User/LoginWrapper";
 import { TERMS_LIST } from "@/constants/mock/JOIN_AGREEMENT";
 import { InputCheckbox } from "@/components/common/SelectInput";
 
-import RightArrowThin from "@/assets/icon/arrow/RightArrowThin.svg";
+import RightArrowIcon from "@/assets/icon/arrow/RightArrowSmall.svg";
 
 function Intro() {
   useBgWhite();
@@ -32,95 +32,97 @@ function Intro() {
   }, [checkedList]);
 
   return (
-    <LoginWrapper
-      button={{
-        text: "확인",
-        onClick: async () => {
-          if (checkedList.required1 && checkedList.required2) {
-            await router.push({
-              pathname: "/user/apply/process",
-              query: {
-                eventAgree: checkedList.event1 ? "T" : "F",
-              },
-            });
-          } else {
-            await confirm?.showConfirm("필수 약관에 모두 동의해야 합니다");
-          }
-        },
-      }}
-    >
-      <Agreement>
-        <AllCheck>
-          <InputCheckbox
-            id="allChecked"
-            checked={allChecked}
-            onChange={(event) => {
-              const isChecked = event.target.checked;
-              setAllChecked(isChecked);
-              setCheckedList({
-                required1: isChecked,
-                required2: isChecked,
-                event1: isChecked,
+    <>
+      <LoginWrapper
+        button={{
+          text: "확인",
+          onClick: async () => {
+            if (checkedList.required1 && checkedList.required2) {
+              await router.push({
+                pathname: "/user/apply/process",
+                query: {
+                  eventAgree: checkedList.event1 ? "T" : "F",
+                },
               });
-            }}
-          />
-          <label htmlFor="allChecked">약관 전체 동의</label>
-        </AllCheck>
-        <TermList>
-          {TERMS_LIST.map((term) => (
-            <li key={term.termId}>
-              <div>
-                <InputCheckbox
-                  id={term.termId}
-                  checked={checkedList[term.termId]}
-                  onChange={(event) => setCheckedList((prev) => ({ ...prev, [term.termId]: event.target.checked }))}
-                />
-                <label htmlFor={term.termId}>
-                  [{term.required ? "필수" : "선택"}] {term.termName}
-                </label>
-              </div>
-              <TermView
-                type="button"
-                aria-label="해당 약관 내용을 자세히"
-                onClick={() => {
-                  showModal();
-                  setSelectedTerm(term.termId);
-                }}
-              >
-                <RightArrowThin />
-              </TermView>
-            </li>
-          ))}
-        </TermList>
-        <ModalComponents
-          title={TERMS_LIST.find((v) => v.termId === selectedTerm)?.termName}
-          buttons={[
-            {
-              mode: "OPTION2",
-              name: "닫기",
-              onClick: (close) => {
-                close();
-              },
+            } else {
+              await confirm?.showConfirm("필수 약관에 모두 동의해야 합니다");
+            }
+          },
+        }}
+      >
+        <Agreement>
+          <AllCheck>
+            <InputCheckbox
+              id="allChecked"
+              checked={allChecked}
+              onChange={(event) => {
+                const isChecked = event.target.checked;
+                setAllChecked(isChecked);
+                setCheckedList({
+                  required1: isChecked,
+                  required2: isChecked,
+                  event1: isChecked,
+                });
+              }}
+            />
+            <label htmlFor="allChecked">약관 전체 동의</label>
+          </AllCheck>
+          <TermList>
+            {TERMS_LIST.map((term) => (
+              <li key={term.termId}>
+                <div>
+                  <InputCheckbox
+                    id={term.termId}
+                    checked={checkedList[term.termId]}
+                    onChange={(event) => setCheckedList((prev) => ({ ...prev, [term.termId]: event.target.checked }))}
+                  />
+                  <label htmlFor={term.termId}>
+                    [{term.required ? "필수" : "선택"}] {term.termName}
+                  </label>
+                </div>
+                <TermView
+                  type="button"
+                  aria-label="해당 약관 내용을 자세히"
+                  onClick={() => {
+                    showModal();
+                    setSelectedTerm(term.termId);
+                  }}
+                >
+                  <RightArrowIcon />
+                </TermView>
+              </li>
+            ))}
+          </TermList>
+        </Agreement>
+      </LoginWrapper>
+      <ModalComponents
+        title={TERMS_LIST.find((v) => v.termId === selectedTerm)?.termName}
+        buttons={[
+          {
+            mode: "OPTION2",
+            name: "닫기",
+            onClick: (close) => {
+              close();
             },
-            {
-              flex: 2,
-              mode: "MAIN",
-              name: "위 약관에 동의",
-              onClick: (close) => {
-                setCheckedList((prev) => ({ ...prev, [selectedTerm]: true }));
-                close();
-              },
+          },
+          {
+            flex: 2,
+            mode: "MAIN",
+            name: "위 약관에 동의",
+            onClick: (close) => {
+              setCheckedList((prev) => ({ ...prev, [selectedTerm]: true }));
+              close();
             },
-          ]}
-        >
-          <TermContents>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum saepe perferendis nam reprehenderit!
-            Necessitatibus culpa quaerat iure vitae qui odit, nihil dolorem velit sapiente perferendis rem alias est
-            consequuntur praesentium.
-          </TermContents>
-        </ModalComponents>
-      </Agreement>
-    </LoginWrapper>
+          },
+        ]}
+      >
+        <TermContents>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum saepe perferendis nam reprehenderit!
+          Necessitatibus culpa quaerat iure vitae qui odit, nihil dolorem velit sapiente perferendis rem alias est
+          consequuntur praesentium.
+        </TermContents>
+      </ModalComponents>
+    </>
   );
 }
 
@@ -169,7 +171,9 @@ const TermView = styled.button`
   height: 16px;
 
   svg {
-    fill: var(--gray700);
+    width: 20px;
+    height: 20px;
+    fill: var(--gray500);
   }
 `;
 const TermContents = styled.div``;

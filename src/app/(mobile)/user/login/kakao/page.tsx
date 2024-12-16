71f1eaab2ff4bd9bd9a913ebe6a44b8205fbd3c9
IRONDESK <zerozerobase@gmail.com>
2024-12-16 23:34:53 +0900

    fix. oauth login 처리 수정

--- src/app/(mobile)/user/login/kakao/page.tsx
+++ src/app/(mobile)/user/login/kakao/page.tsx
@@ -1,23 +1,31 @@
-import React from "react";
-import { redirect } from "next/navigation";
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
 import { setCookie } from "cookies-next";
 
 import { baseBackendURL } from "@/apis";
+import { useGet } from "@/apis/hook/query";
+import { AuthResponseType } from "@/types/auth";
 
-async function KakaoLogin({ params }: { params: Promise<{ code: string }> }) {
-  const { code } = await params;
+function KakaoLogin() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
   const kakaoUrl = `${baseBackendURL}/api/login/koauth2?code=${encodeURIComponent(`${code}`)}`;
-  const data = await fetch(kakaoUrl);
-  const response = await data.json();
-  await setCookie("access-token", response.access_token, { secure: true });
 
-  if ((await response.newUserYn) === "Y") {
-    // 가입되지 않은 회원
-    redirect("/user/apply?from=kakao");
-  } else {
-    // 가입된 회원
-    redirect("/");
+  const { data } = useGet<AuthResponseType>(kakaoUrl);
+
+  if (data) {
+    setCookie("access-token", data.access_token, { secure: true });
+    if (data.newUserYn === "Y") {
+      // 가입되지 않은 회원
+      router.replace("/user/apply?from=google");
+    } else {
+      // 가입된 회원
+      router.replace("/");
+    }
   }
+
+  return <></>;
 }
 
 export default KakaoLogin;
---
+++ src/types/auth.ts
@@ -0,0 +1,10 @@
+export type AuthResponseType = {
+  access_token: string;
+  expires_in: number;
+  id_token: string;
+  newUserYn: string;
+  refresh_token: string;
+  refresh_token_expires_in: number;
+  scope: string;
+  token_type: string;
+};

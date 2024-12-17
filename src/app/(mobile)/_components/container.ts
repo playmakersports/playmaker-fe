8dd81a148c7741109cbba83e54934018125db744
IRONDESK <zerozerobase@gmail.com>
2024-12-17 21:03:25 +0900

    fix. home 컴포넌트 파일 분리

---
+++ src/app/(mobile)/_components/container.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+import { BaseContainer } from "@/components/common/Container";
+
+export const HomeContainer = styled(BaseContainer)`
+  position: relative;
+  margin-top: calc(-1 * var(--header-height));
+  padding: calc(16px + var(--header-height)) 16px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background-color: var(--background);
+`;
+
+export const HomeBackGradient = styled.div`
+  position: absolute;
+  margin: calc(-1 * var(--header-height)) -16px 0;
+  top: 0;
+  width: 100%;
+  height: 900px;
+  background: linear-gradient(165deg, rgba(48, 108, 239, 0.8) 20%, rgb(48, 109, 239, 0) 90%);
+  z-index: 0;
+  opacity: 0.15;
+`;
---
+++ src/app/(mobile)/user/login/univ/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+function LoginError() {
+  return <div>LoginError</div>;
+}
+
+export default LoginError;

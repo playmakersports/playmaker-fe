70433257634fe7da8921c95d2141ee3614e4f8d5
IRONDESK <zerozerobase@gmail.com>
2024-12-28 16:21:45 +0900

    feat. gray 스타일 디바이더 추가

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -12,6 +12,13 @@ export const WhiteSectionDivider = styled.div`
   background-color: var(--background);
   box-shadow: inset 0 4px 4px 0 rgba(195, 220, 243, 0.5);
 `;
+export const WhiteSectionGrayDivider = styled.div`
+  display: block;
+  margin: 0 -16px;
+  width: calc(100% + 32px);
+  height: 12px;
+  background-color: var(--gray100);
+`;
 export const WhiteSectionWrapper = styled.article`
   margin-left: -16px;
   margin-right: -16px;

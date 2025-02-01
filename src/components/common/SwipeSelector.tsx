c159495aa049e385a81db8ecbf6c461f80bafd9e
IRONDESK <zerozerobase@gmail.com>
2025-02-02 02:03:24 +0900

    fix. SwipeSelector flicking 라이브러리 적용 수정

--- src/components/common/SwipeSelector.tsx
+++ src/components/common/SwipeSelector.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+"use client";
+import React, { useRef } from "react";
+import "@egjs/react-flicking/dist/flicking.css";
 import styled from "styled-components";
-import { FONTS } from "@/styles/common";
+import Flicking from "@egjs/react-flicking";
 
 type ButtonsProps = { svg: React.ReactNode; bgColor: string; text: string; onClick: () => void };
 type Props = {
@@ -10,109 +12,69 @@ type Props = {
 };
 function SwipeSelector(props: Props) {
   const { children, left, right } = props;
-  const [width, setWidth] = useState(0);
-  const [activeButton, setActiveButton] = useState(false);
-  const [touchX, setTouchX] = useState(0);
-  const [touchStartX, setTouchStartX] = useState(0);
-
-  const handleTouchStart = (e: React.TouchEvent) => {
-    const startTarget = e.changedTouches[0].pageX;
-    setTouchStartX(startTarget);
-  };
-
-  const handleShowButton = (targetX: number) => {
-    if (touchStartX < targetX) {
-      setTouchX(width / 2 + 10);
-      setActiveButton(true);
-    } else {
-      setTouchX(-1 * (width / 2 - 60));
-      setActiveButton(true);
-    }
-  };
-  const handleTouchEnd = (e: React.TouchEvent) => {
-    const targetX = e.changedTouches[0].pageX;
-    if (!activeButton && Math.abs(touchStartX - targetX) > 20) {
-      handleShowButton(targetX);
-    } else {
-      setTouchX(0);
-      setTouchStartX(0);
-      setActiveButton(false);
-    }
-  };
-
-  const handleTouchMove = (e: React.TouchEvent) => {
-    const targetX = e.changedTouches[0].pageX;
-    if (!activeButton && Math.abs(touchStartX - targetX) > 20 && targetX % 4 === 0) {
-      setTouchX(touchStartX < targetX ? targetX : -1 * (width - targetX));
-    }
-  };
+  const flickRef = useRef<Flicking>(null);
 
   return (
-    <Container ref={(ref) => setWidth(ref?.clientWidth ?? 0)} $moveX={touchX}>
-      <div className="inner-wrapper">
-        <Buttons $bgColor={left[0].bgColor}>
+    <Container>
+      <Flicking
+        ref={flickRef}
+        bounce="0"
+        threshold={50}
+        circular={false}
+        bound={true}
+        defaultIndex={1}
+        useFractionalSize
+      >
+        <SidePanel>
           {left.map((btn) => (
             <Button key={btn.bgColor} $bgColor={btn.bgColor} onClick={btn.onClick}>
               {btn.svg}
               <span>{btn.text}</span>
             </Button>
           ))}
-        </Buttons>
-        <Displayed onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
-          {children}
-        </Displayed>
-        <Buttons $bgColor={right.bgColor}>
+        </SidePanel>
+        <CenterPanel>{children}</CenterPanel>
+        <SidePanel>
           <Button $bgColor={right.bgColor} onClick={right.onClick}>
             {right.svg}
             <span>{right.text}</span>
           </Button>
-        </Buttons>
-      </div>
+        </SidePanel>
+      </Flicking>
     </Container>
   );
 }
 
-const Container = styled.div<{ $moveX: number }>`
+const Container = styled.div`
   position: relative;
-  left: calc(-50% - 150px / 2);
-  ${FONTS.MD1};
-  .inner-wrapper {
-    width: calc(200% + 150px);
-    display: flex;
-    flex-wrap: nowrap;
-    transform: translate3d(${({ $moveX }) => $moveX}px, 0, 0);
-    transition: transform 0.3s;
-    overflow: hidden;
-  }
-  user-select: none;
-
-  &:last-of-type {
-    .inner-wrapper {
-      border-bottom: none;
-    }
-  }
+  height: 92px;
 `;
 
-const Displayed = styled.div`
+const CenterPanel = styled.div`
   flex-shrink: 0;
+  display: flex;
+  align-items: center;
   width: min(var(--mobile-max-width), 100vw);
-  padding: 16px;
+  height: 92px;
+  padding: 0 16px;
   background-color: var(--background-light);
+  & > div {
+    flex: 1;
+  }
 `;
-const Buttons = styled.div<{ $bgColor: string }>`
+const SidePanel = styled.div`
   position: relative;
-  display: inline-flex;
+  display: flex;
+  height: 92px;
   justify-content: flex-end;
-  width: 50%;
-  background-color: ${({ $bgColor }) => $bgColor};
   &:last-of-type {
     justify-content: flex-start;
   }
   &:first-of-type::after {
     content: "";
     position: absolute;
-    right: 0;
-    width: 10px;
+    right: -1px;
+    width: 11px;
     height: 100%;
     background: var(--background-light);
     border-radius: 10px 0 0 10px;
@@ -120,8 +82,8 @@ const Buttons = styled.div<{ $bgColor: string }>`
   &:last-of-type::before {
     content: "";
     position: absolute;
-    left: 0;
-    width: 10px;
+    left: -1px;
+    width: 11px;
     height: 100%;
     background: var(--background-light);
     border-radius: 0 10px 10px 0;
@@ -129,6 +91,7 @@ const Buttons = styled.div<{ $bgColor: string }>`
 `;
 const Button = styled.button<{ $bgColor: string }>`
   display: flex;
+  min-width: 120px;
   padding: 0 22px;
   flex-direction: column;
   justify-content: center;
@@ -138,6 +101,7 @@ const Button = styled.button<{ $bgColor: string }>`
   color: var(--gray0);
   font-size: 1.4rem;
   font-weight: 500;
+  word-break: keep-all;
 
   svg {
     width: 24px;

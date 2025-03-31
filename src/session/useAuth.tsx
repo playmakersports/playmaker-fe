e2a6bb028e35db52d14fec6b19c01c062138f430
IRONDESK <zerozerobase@gmail.com>
2025-03-31 21:36:34 +0900

    fix. useAuth hook 변경 void가 아닌 cookie-next 함수를 반환하도록 수정

--- src/session/useAuth.tsx
+++ src/session/useAuth.tsx
@@ -3,22 +3,20 @@ import { setCookie, deleteCookie, getCookie } from "cookies-next";
 export const useAuth = () => {
   const accessToken = getCookie("access-token") ?? null;
 
-  const setAuthToken = (newToken: string) => {
+  const setAuthToken = (newToken: string) =>
     setCookie("access-token", newToken, {
       path: "/",
       httpOnly: true,
       secure: true,
       sameSite: "lax",
     });
-  };
 
-  const clearAuthToken = () => {
+  const clearAuthToken = () =>
     deleteCookie("access-token", {
       path: "/",
       expires: new Date(0),
     });
-    setCookie("access-token", null);
-  };
+  setCookie("access-token", null);
 
   return {
     accessToken,

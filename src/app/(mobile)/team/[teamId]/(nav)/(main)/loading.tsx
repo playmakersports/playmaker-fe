23e0330209de866fbfaa17ab19a64e8acc2aa94d
IRONDESK <zerozerobase@gmail.com>
2025-04-01 22:24:16 +0900

    fix. 스켈레톤 UI 수정 및 클래스명 변경

--- src/app/(mobile)/team/[teamId]/(nav)/(main)/loading.tsx
+++ src/app/(mobile)/team/[teamId]/(nav)/(main)/loading.tsx
@@ -4,7 +4,7 @@ function TeamMainLoading() {
   return (
     <>
       <div
-        className="loading-ui"
+        className="skeleton-loading-ui"
         style={{
           marginTop: "calc(-1 * var(--safe-area-top))",
           width: "100%",
@@ -12,10 +12,10 @@ function TeamMainLoading() {
         }}
       />
       <div style={{ display: "flex", marginTop: "22px", padding: "24px 20px" }}>
-        <div className="loading-ui" style={{ width: "74px", height: "74px", borderRadius: "50%" }} />
+        <div className="skeleton-loading-ui" style={{ width: "74px", height: "74px", borderRadius: "50%" }} />
         <div style={{ display: "flex", flexDirection: "column", gap: "10px", margin: "4px 0px 0px 20px" }}>
-          <div className="loading-ui" style={{ width: "170px", height: "20px", borderRadius: "5px" }} />
-          <div className="loading-ui" style={{ width: "170px", height: "20px", borderRadius: "5px" }} />
+          <div className="skeleton-loading-ui" style={{ width: "170px", height: "20px", borderRadius: "5px" }} />
+          <div className="skeleton-loading-ui" style={{ width: "170px", height: "20px", borderRadius: "5px" }} />
         </div>
       </div>
     </>
--- src/app/(mobile)/team/[teamId]/(nav)/(sub)/loading.tsx
+++ src/app/(mobile)/team/[teamId]/(nav)/(sub)/loading.tsx
@@ -4,9 +4,9 @@ function TeamLoading() {
   return (
     <>
       <div style={{ display: "flex", flexDirection: "column", gap: "10px", margin: "28px 16px 0" }}>
-        <div className="loading-ui" style={{ width: "320px", height: "20px", borderRadius: "5px" }} />
-        <div className="loading-ui" style={{ width: "150px", height: "20px", borderRadius: "5px" }} />
-        <div className="loading-ui" style={{ width: "220px", height: "20px", borderRadius: "5px" }} />
+        <div className="skeleton-loading-ui" style={{ width: "320px", height: "20px", borderRadius: "5px" }} />
+        <div className="skeleton-loading-ui" style={{ width: "150px", height: "20px", borderRadius: "5px" }} />
+        <div className="skeleton-loading-ui" style={{ width: "220px", height: "20px", borderRadius: "5px" }} />
       </div>
     </>
   );

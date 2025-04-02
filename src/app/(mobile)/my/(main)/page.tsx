25f87fba0584c1ed6bd48f35e08faff959e85ffb
IRONDESK <zerozerobase@gmail.com>
2025-04-02 15:42:42 +0900

    fix. 로그인 및 마이페이지 로직 분기 처리 수정

--- src/app/(mobile)/my/(main)/page.tsx
+++ src/app/(mobile)/my/(main)/page.tsx
@@ -1,17 +1,19 @@
 "use client";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useRouter } from "next/navigation";
-import { getCookie, hasCookie } from "cookies-next";
+import { getCookie } from "cookies-next";
 import MyTabHome from "./home/_page";
 
 function MyTabMain() {
   const router = useRouter();
-  const token = getCookie("access-token");
-  const hasToken = hasCookie("access-token");
 
-  if (!hasToken && !token) {
-    router.replace("/user/login");
-  }
+  useLayoutEffect(() => {
+    const token = getCookie("access-token");
+    if (!token) {
+      router.replace("/user/login");
+    }
+  }, [router]);
+
   return <MyTabHome />;
 }
 
--- src/app/(mobile)/user/login/google/page.tsx
+++ src/app/(mobile)/user/login/google/page.tsx
@@ -1,35 +1,56 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useAuth } from "@/session/useAuth";
+import { usePopup } from "@/components/common/global/PopupProvider";
 
 import { baseBackendURL } from "@/apis";
 import Loading from "@/components/common/Loading";
 
 function GoogleLogin() {
   const router = useRouter();
+  const popup = usePopup();
   const { setToken } = useAuth();
   const searchParams = useSearchParams();
   const code = searchParams.get("code") as string;
   const googleUrl = `${baseBackendURL}/api/login/goauth2?code=${encodeURIComponent(code)}`;
 
   const handleLogin = async () => {
-    const response = await fetch(googleUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => {
+      controller.abort();
+    }, 20000); // 20 seconds
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const data = await response.json();
+    try {
+      const response = await fetch(googleUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        signal: controller.signal,
+      });
+      clearTimeout(timeoutId);
 
-    await setToken(data.access_token);
-    if (data.newUserYn === "Y") {
-      router.replace("/user/apply?from=google");
-    } else {
-      router.replace("/");
+      if (!response.ok) {
+        await popup?.alert(`서버 통신에 실패했어요.`, { title: "로그인 실패" });
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+      await setToken(data.access_token);
+
+      if (data.newUserYn === "Y") {
+        router.replace("/user/apply?from=google");
+      } else {
+        router.replace("/");
+      }
+    } catch (error: any) {
+      if (error.name === "AbortError") {
+        await popup?.alert(`서버와의 통신에 문제가 생겼어요.\n잠시후 다시 시도해주세요.`, {
+          title: "로그인 실패",
+        });
+      } else {
+        throw error;
+      }
     }
   };
 

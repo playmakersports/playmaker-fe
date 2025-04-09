129ee8d234644884040f3f4a14fd58c371a877a1
IRONDESK <zerozerobase@gmail.com>
2025-04-09 14:35:31 +0900

    fix. Header VE 교체 및 로고 변경 반영

--- src/components/layouts/Header/MainHeader.tsx
+++ src/components/layouts/Header/MainHeader.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import styled from "styled-components";
 import { useRouter } from "next/navigation";
+import clsx from "clsx";
 
-import { HeaderIcon, HeaderInner, HeaderWrapper } from ".";
-import Logotype from "@/assets/logo/Logotype.svg";
+import {
+  headerButtonDarkBgIconWrapper,
+  headerButtonIcon,
+  headerButtonIconWrapper,
+  headerContainer,
+  headerInnerContainer,
+  headerMainContainer,
+  headerMainLogoContainer,
+  headerMainScrolledContainer,
+} from "./header.css";
+
+import LogoSymbol from "@/assets/logo/LogoSymbol.svg";
+import LogoSymbolGreen from "@/assets/logo/LogoSymbolGreen.svg";
+import LogoTextType from "@/assets/logo/LogoTextType.svg";
+
 import NoticeBellIcon from "@/assets/icon/common/filled/Notification.svg";
 import NoticeBellActiveIcon from "@/assets/icon/common/filled/NotificationUnread.svg";
 import PersonIcon from "@/assets/icon/common/filled/Person.svg";
@@ -14,44 +27,25 @@ function MainHeader({ scrollY }: Props) {
   const isScrolled = scrollY > 160;
 
   return (
-    <Wrapper $isScrolled={isScrolled}>
-      <Inner>
-        <Logotype width={140} height={32} fill={isScrolled ? "var(--gray800)" : "var(--gray100)"} />
-        <Menu>
-          <HeaderIconWrapper onClick={() => router.push("/notification")} aria-label="내 알림 전체보기">
-            <NoticeBellIcon fill={isScrolled ? "var(--gray800)" : "var(--gray100)"} />
-          </HeaderIconWrapper>
-        </Menu>
-      </Inner>
-    </Wrapper>
+    <header className={clsx(headerContainer, headerMainContainer, isScrolled && headerMainScrolledContainer)}>
+      <div className={headerInnerContainer}>
+        <div className={headerMainLogoContainer}>
+          {isScrolled ? <LogoSymbolGreen width={40} height={28} /> : <LogoSymbol width={40} height={28} />}
+          <LogoTextType width={132} height={20} fill={isScrolled ? "var(--gray700)" : "var(--gray100)"} />
+        </div>
+        <div style={{ display: "flex", gap: "12px" }}>
+          <button
+            type="button"
+            className={clsx(headerButtonIconWrapper, !isScrolled && headerButtonDarkBgIconWrapper)}
+            onClick={() => router.push("/notification")}
+            aria-label="내 알림 전체보기"
+          >
+            <NoticeBellIcon className={headerButtonIcon} fill={isScrolled ? "var(--gray700)" : "var(--gray100)"} />
+          </button>
+        </div>
+      </div>
+    </header>
   );
 }
-
-const Wrapper = styled(HeaderWrapper)<{ $isScrolled: boolean }>`
-  position: fixed;
-  background-color: ${({ $isScrolled }) => ($isScrolled ? "var(--background-light)" : "rgba(0,0,0,0.25)")};
-  backdrop-filter: ${({ $isScrolled }) => ($isScrolled ? "none" : "blur(6px)")};
-
-  @media (max-width: 540px) {
-    max-width: 100%;
-  }
-`;
-const HeaderIconWrapper = styled(HeaderIcon)`
-  svg {
-    width: 24px;
-    height: 24px;
-  }
-`;
-const Inner = styled(HeaderInner)`
-  padding-left: 12px;
-  padding-right: 12px;
-`;
-const Menu = styled.div`
-  display: flex;
-  gap: 12px;
-  font-weight: 400;
-  font-size: 1.6rem;
-  color: var(--gray900);
-`;
 
 export default MainHeader;
---
+++ src/components/layouts/Header/header.css.ts
@@ -0,0 +1,74 @@
+import { style } from "@vanilla-extract/css";
+
+export const headerContainer = style({
+  position: "fixed",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  maxWidth: "var(--mobile-max-width)",
+  top: "0",
+  left: "50%",
+  transform: "translateX(-50%)",
+  padding: "env(safe-area-inset-top) 16px",
+  height: "var(--header-height)",
+  zIndex: 999,
+  transition: "background-color 0.3s, backdrop-filter 0.3s",
+});
+export const headerMainContainer = style([
+  headerContainer,
+  {
+    backgroundColor: "rgba(0,0,0,0.25)",
+    // backdropFilter: "blur(6px)",
+    "@media": {
+      "screen and (max-width: 540px)": {
+        maxWidth: "100%",
+      },
+    },
+  },
+]);
+export const headerMainScrolledContainer = style({
+  backgroundColor: "var(--background-light)",
+  backdropFilter: "none",
+});
+export const headerMainLogoContainer = style({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+});
+export const headerInnerContainer = style({
+  display: "flex",
+  width: "100%",
+  flexShrink: 0,
+  alignItems: "center",
+  justifyContent: "space-between",
+  gap: "20px",
+  padding: "env(safe-area-inset-top) 4px 0",
+});
+
+export const headerButtonIconWrapper = style({
+  display: "inline-flex",
+  width: "24px",
+  height: "24px",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "2px",
+  selectors: {
+    "&:active": {
+      backgroundColor: "var(--gray100)",
+      boxShadow: "0 0 0 6px var(--gray100)",
+    },
+  },
+});
+export const headerButtonDarkBgIconWrapper = style({
+  selectors: {
+    "&:active": {
+      backgroundColor: "var(--gray800)",
+      boxShadow: "0 0 0 6px var(--gray800)",
+    },
+  },
+});
+export const headerButtonIcon = style({
+  width: "24px",
+  height: "24px",
+});

42f8c0b02d5e990c2f26e25150ec486652942061
IRONDESK <zerozerobase@gmail.com>
2025-04-24 01:23:12 +0900

    fix. 팀 헤더 수정 팀 관리 링크 추가, 살펴보기 버튼 추가

--- src/components/layouts/Header/TeamHeader.tsx
+++ src/components/layouts/Header/TeamHeader.tsx
@@ -1,20 +1,25 @@
 "use client";
 import React, { useState } from "react";
 import { useAtomValue } from "jotai";
-import { usePathname } from "next/navigation";
+import { usePathname, useParams, useRouter } from "next/navigation";
+import Link from "next/link";
 import clsx from "clsx";
 import styled from "styled-components";
 
 import { atomHeaderTransparent, atomPageTitle } from "@/atom/common";
 import { RoutedHeaderContainer } from "@/components/layouts/Header/RoutedHeader";
+import Badge from "@/components/common/Badge";
 import DropdownAction from "@/components/common/input/DropdownAction";
 import HeaderTeamMover from "@/app/(mobile)/team/_components/HeaderTeamMover";
+import SearchIcon from "@/assets/icon/common/Search.svg";
 
 type Props = {
   scrollY: number;
 };
 function TeamHeader({ scrollY }: Props) {
   const pathname = usePathname();
+  const teamId = useParams()["teamId"];
+  const router = useRouter();
   const title = useAtomValue(atomPageTitle);
   const bgTransparent = useAtomValue(atomHeaderTransparent);
   const DEFAULT_SCROLLED_Y = 160;
@@ -42,27 +47,50 @@ function TeamHeader({ scrollY }: Props) {
           <HeaderTeamMover title={title} showList={showList} setShowList={setShowList} />
         </div>
         {pathname !== "/team/find" && (
-          <ActionMenu className={clsx({ hide: showList })}>
-            <DropdownAction
-              icon
-              options={[
-                { name: "팀 관리", action: () => {} },
-                { name: "팀 탈퇴", action: () => {} },
-              ]}
-            />
-          </ActionMenu>
+          <RightSection>
+            <div className={clsx("action-menu", { hide: showList })}>
+              <DropdownAction
+                icon
+                options={[
+                  {
+                    name: "팀 관리",
+                    action: () => {
+                      router.push(`/team/${teamId}/admin`);
+                    },
+                  },
+                  { name: "팀 탈퇴", action: () => {} },
+                ]}
+              />
+            </div>
+            <div className={clsx("action-menu", { hide: !showList })}>
+              <Link href="/team/find" onClick={() => setShowList(false)}>
+                <Badge type="gray" fillType="light" size="large" icon={<SearchIcon />}>
+                  팀 살펴보기
+                </Badge>
+              </Link>
+            </div>
+          </RightSection>
         )}
       </RoutedHeaderContainer>
     </header>
   );
 }
 
-const ActionMenu = styled.div`
-  transition: transform 0.25s ease-in-out, opacity 0.2s ease-in-out;
-  &.hide {
-    will-change: transform;
-    transform: translate3d(180%, 0, 0);
-    opacity: 0;
+const RightSection = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: flex-end;
+  grid-template-areas: "overlap";
+
+  div.action-menu {
+    user-select: none;
+    grid-area: overlap;
+    transition: transform 0.25s ease-in-out, opacity 0.2s ease-in-out;
+    &.hide {
+      will-change: transform;
+      transform: translate3d(180%, 0, 0);
+      opacity: 0;
+    }
   }
 `;
 

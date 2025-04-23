c08ab6cfcebb09d0e811a0494689f7877cd513b0
IRONDESK <zerozerobase@gmail.com>
2025-04-24 01:21:56 +0900

    fix. flex-column gap 공통 css 생성

--- src/styles/container.css.ts
+++ src/styles/container.css.ts
@@ -3,3 +3,28 @@ import { style } from "@vanilla-extract/css";
 export const baseContainer = style({
   padding: "0 20px var(--safe-bottom-navigation)",
 });
+export const flexColumnGap40 = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "40px",
+});
+export const flexColumnGap24 = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "24px",
+});
+export const flexColumnGap20 = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+});
+export const flexColumnGap16 = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+});
+export const flexColumnGap12 = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+});

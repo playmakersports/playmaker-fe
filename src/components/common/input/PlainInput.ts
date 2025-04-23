bd0b979fbb5b62d62cc3389df7ac360c70a6b16f
IRONDESK <zerozerobase@gmail.com>
2025-04-24 01:18:33 +0900

    feat. Range Input 분리

--- src/components/common/input/PlainInput.ts
+++ src/components/common/input/PlainInput.ts
@@ -19,3 +19,40 @@ export const DateKeypadInput = styled.input`
     border-radius: 4px;
   }
 `;
+
+export const RangeSliderInput = styled.input<{
+  min: number;
+  max: number;
+  value: number;
+}>`
+  appearance: none;
+  width: 100%;
+  height: 2px;
+
+  background: ${({ min, max, value }) => {
+    const percentage = ((value - min) / (max - min)) * 100;
+    return `linear-gradient(to right, var(--primary500) 0%, var(--primary500) ${percentage}%, var(--gray200) ${percentage}%, var(--gray200) 100%)`;
+  }};
+  border-radius: 4px;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 16px;
+    height: 16px;
+    background-color: var(--white);
+    border: 1px solid var(--gray300);
+    border-radius: 50%;
+    cursor: pointer;
+    box-shadow: var(--shadow-xs);
+  }
+  &::-moz-range-thumb {
+    width: 16px;
+    height: 16px;
+    background-color: var(--white);
+    border: 1px solid var(--gray300);
+    border-radius: 50%;
+    cursor: pointer;
+    box-shadow: var(--shadow-xs);
+  }
+`;

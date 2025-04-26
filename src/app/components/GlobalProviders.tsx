f4a70da424df6b077763098819f0fd881f6d0bec
IRONDESK <zerozerobase@gmail.com>
2025-04-26 17:39:49 +0900

    fix. toast bottom offset 변경

--- src/app/components/GlobalProviders.tsx
+++ src/app/components/GlobalProviders.tsx
@@ -23,11 +23,12 @@ function GlobalProviders({ children }: { children: React.ReactNode }) {
       <SoonerToaster
         position="bottom-center"
         duration={1970}
+        offset={{ bottom: "calc(var(--safe-bottom-navigation) + 20px)" }}
         mobileOffset={{
           top: 16,
           right: 16,
           left: 16,
-          bottom: "calc(var(--safe-bottom) + 20px)",
+          bottom: "calc(var(--safe-bottom-navigation) + 20px)",
         }}
       />
       <QueryClientProvider client={queryClient}>

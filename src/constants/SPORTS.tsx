e0f36860c08f7adfc999702387f90737f468b43c
IRONDESK <zerozerobase@gmail.com>
2025-05-04 19:49:02 +0900

    fix. 스포츠 상수 nameEng 속성 추가

--- src/constants/SPORTS.tsx
+++ src/constants/SPORTS.tsx
@@ -6,21 +6,24 @@ import FootballIcon from "@/assets/icon/sports/football.png";
 import BadmintonIcon from "@/assets/icon/sports/badminton.png";
 import RunIcon from "@/assets/icon/sports/run.png";
 
-type SportsInfoType = { value: string; name: string; icon: StaticImageData };
+type SportsInfoType = { value: string; name: string; nameEng: string; icon: StaticImageData };
 export const SUPPORT_SPORTS: Array<SportsInfoType> = [
   {
     value: "1",
     name: "농구",
+    nameEng: "basketball",
     icon: BasketballIcon,
   },
   {
     value: "2",
     name: "축구",
+    nameEng: "football",
     icon: FootballIcon,
   },
   {
     value: "3",
     name: "배구",
+    nameEng: "volleyball",
     icon: VolleyballIcon,
   },
   // {
@@ -31,6 +34,7 @@ export const SUPPORT_SPORTS: Array<SportsInfoType> = [
   {
     value: "4",
     name: "야구",
+    nameEng: "baseball",
     icon: RunIcon,
   },
 ];

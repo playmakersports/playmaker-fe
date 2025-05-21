c0fad49cb8f9a412480cc1fe67e6a1eaa9091099
IRONDESK <zerozerobase@gmail.com>
2025-05-22 02:57:43 +0900

    fix. useDeviceAgent isIOS, isAndroid 추가

--- src/hook/useDeviceAgent.tsx
+++ src/hook/useDeviceAgent.tsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 
 function useDeviceAgent() {
   const [isMobile, setIsMobile] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
+  const [isAndroid, setIsAndroid] = useState(false);
 
   useEffect(() => {
     const userAgent = navigator.userAgent;
     setIsMobile(/iPhone|iPad|iPod|Android/i.test(userAgent));
+    setIsIOS(/iPhone|iPad|iPod/i.test(userAgent));
+    setIsAndroid(/Android/i.test(userAgent));
   }, []);
 
-  return { isMobile };
+  return { isMobile, isIOS, isAndroid };
 }
 
 export default useDeviceAgent;

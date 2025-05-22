a8be1ee534584a7922e3b97a78a4184059639972
IRONDESK <zerozerobase@gmail.com>
2025-05-22 15:39:25 +0900

    feat. 우측 하단 플러스 버튼 컴포넌트로 분리

---
+++ src/components/common/PlusFloat.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+import PlusIcon from "@/assets/icon/common/Plus.svg";
+
+function PlusFloat({ linkTo, blind }: { linkTo: string; blind?: string }) {
+  return (
+    <Link
+      prefetch={true}
+      href={linkTo}
+      style={{
+        backgroundColor: "var(--primary500)",
+        borderRadius: "50%",
+        position: "absolute",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: 48,
+        height: 48,
+        marginBottom: "10px",
+        right: "var(--global-lr-padding)",
+        bottom: "var(--safe-bottom-navigation)",
+        boxShadow: "var(--shadow-sm)",
+        zIndex: 5,
+      }}
+    >
+      {blind && <span className="blind">{blind}</span>}
+      <PlusIcon width={24} height={24} fill="var(--white)" />
+    </Link>
+  );
+}
+
+export default PlusFloat;

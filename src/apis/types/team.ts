fc90ae9abcf0cd31a3f2a3535a616ce79e3e58ff
IRONDESK <zerozerobase@gmail.com>
2025-06-08 18:05:30 +0900

    fix. api type 수정 반영

---
+++ src/apis/types/team.ts
@@ -0,0 +1,20 @@
+export type ApiTeamDetail = {
+  id: number;
+  teamName: string;
+  teamColor: string | null;
+  teamItem: string;
+  createDate: string;
+  activeArea: string;
+  logoUrl: string | null;
+  teamIntro: string | null;
+  message: string | null;
+  joinYn: "Y" | "N" | null;
+  publicYn: "Y" | "N" | null;
+  recruitingYn: "Y" | "N" | null;
+  showAgeLimitYn: "Y" | "N" | null;
+  showGenderYn: "Y" | "N" | null;
+  showFoundingDateYn: "Y" | "N" | null;
+  deleteScheduledDate: string | null;
+  teamLeaderId: number;
+  teamLeaderName: string;
+};
--- src/types/team.ts
+++ src/types/team.ts
@@ -1,23 +1,3 @@
-export type SelectTeamResponse = {
-  teamId: number;
-  teamName: string;
-  masterNm: string;
-  createDt: string;
-  item: string;
-  logoUrl: string;
-  bgUrl: string;
-  joinYn: "Y" | "N";
-  activeArea: string;
-  university: string;
-  teamIntro: string;
-  maxBirthYear: number | null;
-  minBirthYear: number | null;
-  publicYn: "Y" | "N";
-  myTeamYn: "Y" | "N";
-  countMember: number;
-  status: number; // 0: 미로그인, 1: 신청가능(가입안된사람), 2: 신청중, 3: 가입불가(블랙), 4: 회원, 5: 운영진, 6: 부회장, 7: 회장
-};
-
 export type PostTeamBoardRequest = {
   boardInfo: {
     category: number;

fb25375c750c10cb578caf01db6e93e99bff6748
IRONDESK <zerozerobase@gmail.com>
2025-06-10 23:47:19 +0900

    fix. onboarding 화면 처리에 pathname 조건 추가

--- src/app/(mobile)/_components/OnboardingRoutes.tsx
+++ src/app/(mobile)/_components/OnboardingRoutes.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { usePathname } from "next/navigation";
 import AppOnboardingHome from "../user/_onboarding";
 import { useAtomValue } from "jotai";
 import { isOnboardingAtom } from "@/session/userAtom";
 
 function OnboardingRoutes({ children }: { children: React.ReactNode }) {
   const onboarding = useAtomValue(isOnboardingAtom);
+  const pathname = usePathname();
+  const isOAuthStart = pathname.startsWith("/user/login");
 
-  if (!onboarding) return <AppOnboardingHome />;
+  if (!onboarding && !isOAuthStart) return <AppOnboardingHome />;
   return children;
 }
 

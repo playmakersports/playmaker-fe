c54cd5973128c9302ca3dff3e84a5c8c5db74ef6
IRONDESK <zerozerobase@gmail.com>
2025-06-15 15:57:55 +0900

    fix. logout시 refresh 처리

--- src/app/(mobile)/user/logout/page.tsx
+++ src/app/(mobile)/user/logout/page.tsx
@@ -2,18 +2,19 @@
 import { useEffect } from "react";
 import { deleteCookie } from "cookies-next";
 import { useSetUser } from "@/session/useSetUser";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function Logout() {
+  const router = useRouter();
   const { logout } = useSetUser();
   useEffect(() => {
     deleteCookie("access-token", {
       path: "/",
     });
     logout();
+    router.replace("/user");
+    router.refresh();
   }, []);
-
-  redirect("/user");
 }
 
 export default Logout;

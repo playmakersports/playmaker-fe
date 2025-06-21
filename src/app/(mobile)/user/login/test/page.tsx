68e0df2ee1d980d9c924b5d166afe18d0ab4cdf0
IRONDESK <zerozerobase@gmail.com>
2025-06-22 00:45:54 +0900

    fix. 테스트 로그인 내부 로직 수정

--- src/app/(mobile)/user/login/test/page.tsx
+++ src/app/(mobile)/user/login/test/page.tsx
@@ -1,58 +1,48 @@
 "use client";
-
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useGet } from "@/apis/hook/query";
-// import { useAuth } from "@/session/useAuth";
-import { useSetUser } from "@/session/useSetUser";
+import { useToast } from "@/hook/useToast";
 
 import Loading from "@/components/common/Loading";
-import { setTokens, clearTokens, getAccessToken } from "@/session/authToken";
+import { setTokens, clearTokens } from "@/session/authToken";
 
 function TestLogin() {
   const router = useRouter();
-  const { logout } = useSetUser();
-
-  // const { setToken, clearToken } = useAuth();
+  const toast = useToast();
 
   const { data, isSuccess, isLoading, error } = useGet<{ access_token: string } & any>(
     "/api/dev/test/random-token",
     {}
   );
-  const once = useRef(false);
 
   useEffect(() => {
-    if (once.current === false) {
-      if (isSuccess) {
-        // setToken(data.access_token);
-        setTokens(data);
-        router.replace("/home");
+    if (isLoading) return;
+    if (data?.access_token && isSuccess) {
+      setTokens(data);
+      toast.trigger("테스트 로그인에 성공했습니다", { type: "success" });
+      router.replace("/home");
+    } else {
+      const confirm = window.confirm(
+        `서버 통신 문제로 테스트 토큰이 발급되지 않았습니다.\n로그인 화면으로 돌아가시겠습니까?`
+      );
+      if (confirm) {
+        clearTokens();
       } else {
-        const confirm = window.confirm(
-          `서버 통신 문제로 테스트 토큰이 발급되지 않았습니다.\n로그인 화면으로 돌아가시겠습니까?`
-        );
-        if (confirm) {
-          clearTokens();
-          logout();
-        } else {
-          setTokens({
-            access_token: "test-access-token-123player-maker",
-            refresh_token: "test-refresh-token-123player-maker",
-            expires_in: 3600,
-            token_type: null,
-            scope: null,
-            refresh_token_expires_in: 10,
-            id_token: null,
-            newUserYn: "N",
-          });
-          console.log("test-token", getAccessToken());
-          router.replace("/home");
-        }
-        // router.replace("/user");
+        setTokens({
+          access_token: "test-access-token-123player-maker",
+          refresh_token: "test-refresh-token-123player-maker",
+          expires_in: 3600,
+          token_type: null,
+          scope: null,
+          refresh_token_expires_in: 10,
+          id_token: null,
+          newUserYn: "N",
+        });
+        router.replace("/home");
       }
-      once.current = true;
     }
-  }, [isSuccess]);
+  }, [isLoading]);
 
   if (isLoading) return <Loading page />;
   return (

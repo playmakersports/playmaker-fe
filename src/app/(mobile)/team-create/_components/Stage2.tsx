fa8fed00196d7cd892c065355217d7a31c82f2b6
IRONDESK <zerozerobase@gmail.com>
2025-06-22 22:38:59 +0900

    fix. 사용되지 않는 import 모듈 제거

--- src/app/(mobile)/team-create/_components/Stage2.tsx
+++ src/app/(mobile)/team-create/_components/Stage2.tsx
@@ -1,5 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import styled from "styled-components";
+import React from "react";
 import { useFormContext } from "react-hook-form";
 
 import { flexColumnGap20 } from "@/styles/container.css";

d5a656ca3713241fcf26b3756b8f020f33aac2ef
IRONDESK <zerozerobase@gmail.com>
2025-06-25 01:19:09 +0900

    fix. 검색 화면 스타일 변경 반영

--- src/app/(mobile)/team/[teamId]/board/_components/SearchPopup.tsx
+++ src/app/(mobile)/team/[teamId]/board/_components/SearchPopup.tsx
@@ -1,88 +1,64 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect } from "react";
+import { useSetAtom } from "jotai";
+import clsx from "clsx";
+
 import { fonts } from "@/styles/fonts.css";
-import SearchIcon from "@/assets/icon/common/Search.svg";
-import CloseIcon from "@/assets/icon/common/Close24.svg";
-import Chip from "@/components/common/Chip";
+import { semantic } from "@/styles/color.css";
+import { flexAlignCenter, flexColumnGap16, flexRowGap10 } from "@/styles/container.css";
+import { atomHeaderDisplay } from "@/atom/common";
+import { BasicInput } from "@/components/common/input/BaseInput";
+import LeftDirection from "@/assets/icon/arrow/LeftDirection.svg";
 
 function SearchPopup({ setShow }: { setShow: React.Dispatch<React.SetStateAction<boolean>> }) {
+  const setDisplayHeader = useSetAtom(atomHeaderDisplay);
   const [keyword, setKeyword] = React.useState("");
+
+  useEffect(() => {
+    setDisplayHeader(false);
+    return () => {
+      setDisplayHeader(true);
+    };
+  }, []);
+
   return (
     <div
       style={{
-        position: "absolute",
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
+        position: "fixed",
+        marginTop: "calc(var(--header-height) * -1)",
+        padding: "0 var(--global-lr-padding)",
         width: "100%",
-        zIndex: 900,
+        maxWidth: "var(--mobile-max-width)",
+        height: "100%",
+        zIndex: 901,
+        backgroundColor: "var(--background-light)",
       }}
     >
-      <div
-        style={{
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-          padding: "28px 20px 20px",
-          backgroundColor: "var(--background-light)",
-          boxShadow: "var(--shadow-lg)",
-        }}
-      >
-        <div
-          role="dialog"
-          style={{
-            display: "flex",
-            gap: "10px",
-          }}
-        >
-          <Input
+      <div className={clsx(flexRowGap10, flexAlignCenter)} style={{ height: "var(--header-height)" }}>
+        <button type="button" aria-label="검색창 닫기" onClick={() => setShow(false)}>
+          <LeftDirection width={24} height={24} />
+        </button>
+        <div style={{ flex: 1 }}>
+          <BasicInput
+            width="100%"
+            iconType="search"
+            delButton={true}
             type="text"
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
-            placeholder="검색어를 입력하세요..."
+            placeholder="검색어를 입력하세요."
             autoFocus={true}
             className={fonts.body3.regular}
           />
-          <button
-            type="button"
-            aria-label="검색"
-            disabled={!keyword}
-            style={{ wordBreak: "keep-all", opacity: keyword ? 1 : 0.4 }}
-          >
-            <Chip type={keyword ? "primary" : "gray"} fillType="filled" size="large">
-              <SearchIcon /> 검색
-            </Chip>
-          </button>
-        </div>
-        <div style={{ flex: 1, display: "flex", alignItems: "flex-end", justifyContent: "center" }}>
-          <button type="button" onClick={() => setShow(false)}>
-            <span className="blind">검색창 닫기</span>
-            <CloseIcon width={32} height={32} fill="var(--gray400)" />
-          </button>
         </div>
       </div>
-      <div
-        onClick={() => setShow(false)}
-        style={{
-          flex: 3,
-          backgroundColor: "rgba(256,256,256,0.5)",
-        }}
-      ></div>
+      <div className={flexColumnGap16} style={{ marginTop: "16px" }}>
+        <p className={fonts.body4.medium}>최근 검색어</p>
+        <p className={semantic.description} style={{ textAlign: "center" }}>
+          최근 검색 내역이 없습니다.
+        </p>
+      </div>
     </div>
   );
 }
-
-const Input = styled.input`
-  width: 100%;
-  padding: 2px 8px;
-  border-bottom: 2px solid var(--gray300);
-  color: var(--gray700);
-  &:focus {
-    border-color: var(--gray400);
-  }
-  &:not(:placeholder-shown) {
-    border-color: var(--primary500);
-  }
-`;
 
 export default SearchPopup;
